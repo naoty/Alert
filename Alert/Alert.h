89c2cee899ad2c0d6ad5c02e4a0701aa1e4fa734
Naoto Kaneko <naoty.k@gmail.com>
2016-02-16 16:08:16 +0900

    Initial commit

---
+++ Alert/Alert.h
@@ -0,0 +1,19 @@
+//
+//  Alert.h
+//  Alert
+//
+//  Created by Naoto Kaneko on 2/16/16.
+//  Copyright © 2016 Naoto Kaneko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Alert.
+FOUNDATION_EXPORT double AlertVersionNumber;
+
+//! Project version string for Alert.
+FOUNDATION_EXPORT const unsigned char AlertVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Alert/PublicHeader.h>
+
+
